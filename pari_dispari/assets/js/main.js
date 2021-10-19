f98d540e8514ea6cc55fdafae7591323a8644114
ChiaraPapagni <chiaraiors@gmail.com>
2021-10-19 15:28:09 +0200

    aggiunto stile css esercizio pari e dispari

--- pari_dispari/assets/js/main.js
+++ pari_dispari/assets/js/main.js
@@ -1,44 +1,68 @@
-//let evenOrOdd = prompt('Pari o Dispari?');
-//let userNumber = Number(prompt('Inserisci un numero da 1 a 5'));
+//seleziono elementi dalla DOM
+const userNumber = document.getElementById('user_number');
+const evenOrOdd = document.getElementById('even_odd');
 
+const playButton = document.getElementById('play');
+
+let pcNumber = 0;
 let sum = 0;
+let message;
 
-/**
- * Questa funzione genera un numero random tra 1 e 5
- *
- * @returns {number} restituisce un numero
- */
-function generateRandomIntegerForPc() {
-    return Math.floor((Math.random() * 5) + 1);
-}
+//al click genera dati numero pc 
+//controlla pari o dispari
+//controlla vincitore
+playButton.addEventListener('click', function () {
 
-let pcNumber = generateRandomIntegerForPc();
+    if (parseInt(userNumber.value) > 5 || parseInt(userNumber.value) == 0) {
+        alert('Devi inserire un numero tra 1 e 5');
+        document.querySelector('.message').innerHTML = ` `;
+        document.querySelector('.winner').innerHTML = ` `;
+    } else {
 
-sum = pcNumber + userNumber;
+        /**
+         * Questa funzione genera un numero random tra 1 e 5
+         *
+         * @returns {number} restituisce un numero
+         */
+        function generateRandomIntegerForPc() {
+            return Math.floor((Math.random() * 5) + 1);
+        }
 
-/**
- * Questa funzione controlla se un numero è pari o dispari
- *
- * @param {number} number - numero
- * @returns {boolean} restituisce true o false
- */
-function isEven(number) {
-    if (number % 2 == 0) {
-        return true;
+        pcNumber = generateRandomIntegerForPc();
+        sum = pcNumber + parseInt(userNumber.value);
+
+        /**
+         * Questa funzione controlla se un numero è pari o dispari
+         *
+         * @param {number} number - numero
+         * @returns {boolean} restituisce true o false
+         */
+        function isEven(number) {
+            if (number % 2 == 0) {
+                return true;
+            }
+            return false;
+        }
+
+
+        if (isEven(sum) && evenOrOdd.value == 'pari') {
+            message = `Vince l'utente!`;
+        } else if (!isEven(sum) && evenOrOdd.value == 'dispari') {
+            message = `Vince l'utente!`;
+        } else {
+            message = `Vince il computer!`;
+        }
+
+
+        console.log(`L' utente ha scelto: ${evenOrOdd.value}`);
+        console.log('Utente: ' + userNumber.value);
+        console.log('Computer: ' + pcNumber);
+        console.log('Somma totale: ' + sum);
+
+        document.querySelector('.message').innerHTML =
+            `Il computer ha scelto il numero ${pcNumber} - La somma totale è ${sum}`;
+        document.querySelector('.winner').innerHTML = message;
+
     }
-    return false;
-}
 
-if (isEven(sum) && evenOrOdd.toLowerCase() == 'pari') {
-    console.log(`Vince l'utente`);
-} else if (!isEven(sum) && evenOrOdd.toLowerCase() == 'dispari') {
-    console.log(`Vince l'utente`);
-} else {
-    console.log(`Vince il computer`);
-}
-
-
-console.log(`L' utente ha scelto: ${evenOrOdd}`);
-console.log('Utente: ' + userNumber);
-console.log('Computer: ' + pcNumber);
-console.log('Somma totale: ' + sum);
+});
