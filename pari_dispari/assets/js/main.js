83cc9982b6d52ccca673705f1064909406a30781
ChiaraPapagni <chiaraiors@gmail.com>
2021-10-19 14:43:01 +0200

    creata funzione per controllare se un numero è pari o dispari aggiunto controllo vincitore

--- pari_dispari/assets/js/main.js
+++ pari_dispari/assets/js/main.js
@@ -1,5 +1,5 @@
-let evenOrOdd = prompt('Pari o Dispari?');
-let userNumber = Number(prompt('Inserisci un numero da 1 a 5'));
+//let evenOrOdd = prompt('Pari o Dispari?');
+//let userNumber = Number(prompt('Inserisci un numero da 1 a 5'));
 
 let sum = 0;
 
@@ -16,3 +16,29 @@ let pcNumber = generateRandomIntegerForPc();
 
 sum = pcNumber + userNumber;
 
+/**
+ * Questa funzione controlla se un numero è pari o dispari
+ *
+ * @param {number} number - numero
+ * @returns {boolean} restituisce true o false
+ */
+function isEven(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+
+if (isEven(sum) && evenOrOdd.toLowerCase() == 'pari') {
+    console.log(`Vince l'utente`);
+} else if (!isEven(sum) && evenOrOdd.toLowerCase() == 'dispari') {
+    console.log(`Vince l'utente`);
+} else {
+    console.log(`Vince il computer`);
+}
+
+
+console.log(`L' utente ha scelto: ${evenOrOdd}`);
+console.log('Utente: ' + userNumber);
+console.log('Computer: ' + pcNumber);
+console.log('Somma totale: ' + sum);
